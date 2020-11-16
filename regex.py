cb0bb2c17696641231561462c913ca2aac9ba562
Roberto Linares <blendboy@gmail.com>
2020-11-16 11:09:57 -0600

    Avances en el script de regex

--- regex.py
+++ regex.py
@@ -1,6 +1,6 @@
 import re
 
-phoneNumberRegex = re.compile(r'\d\d\d-\d\d\d-\d\d\d\d')
+phoneNumberRegex = re.compile(r'(\d\d\d)-(\d\d\d)-(\d\d\d\d'))
 
 mo = phoneNumberRegex.search('My number is 415-555-4242.')
 print('Phone number found: ' + mo.group())
