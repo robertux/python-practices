3bb5c4808ef748da861724560253f04facbd2ccc
Roberto Linares <blendboy@gmail.com>
2020-12-13 16:22:53 -0600

    Cambios finales para el script

--- tareaFinal.py
+++ tareaFinal.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python
+import re
 
 class Empleado:
 
@@ -6,51 +7,52 @@ class Empleado:
 		self.dui = dui
 		self.nit = nit
 		self.sueldo = sueldo
-		self.tiempo_trabajo = tiempo_trabajo #{"anios": 0, "meses": 0, "dias": 0}
+		self.tiempo_trabajo = tiempo_trabajo #en años
+		self.aguinaldo = self.calcular_aguinaldo()
 
 	def datos_validos(self):
-		if (len(self.dui) != 9):
-			print("Longitud incorrecta de DUI: ", len(str(self.dui)))
+		if (not re.match("\d{9}", self.dui)):
+			print("DUI debe ser un valor numérico de 9 dígitos: ", self.dui)
 			return False
 
-		if (len(self.nit) != 14):
-			print("Longitud incorrecta de NIT: ", len(str(self.nit)))
+		if (not re.match("\d{14}", self.nit)):
+			print("NIT debe ser un valor numérico de 14 dígitos: ", self.nit)
 			return False
 
 		if (self.sueldo <= 0.00):
 			print("Sueldo debe ser un valor positivo: ", self.sueldo)
 			return False
 
-		if (self.tiempo_trabajo["anios"] < 0):
-			print("Los anios de trabajo debe ser un valor positivo: ", self.tiempo_trabajo["anios"])
-			return False
-
-		if (self.tiempo_trabajo["meses"] < 0):
-			print("Los meses de trabajo debe ser un valor positivo: ", self.tiempo_trabajo["meses"])
-			return False
-
-		if (self.tiempo_trabajo["dias"] < 0):
-			print("Los dias de trabajo debe ser un valor positivo: ", self.tiempo_trabajo["dias"])
+		if (self.tiempo_trabajo < 0):
+			print("Los anios de trabajo debe ser un valor positivo: ", self.tiempo_trabajo)
 			return False
 
 		return True
 
 	def to_string(self):
-		print(f'Empleado[dui: {self.dui[:8]}-{self.dui[8]}, nit: {self.nit[:4]}-{self.nit[4:10]}-{nit[10:13]}-{nit[13]}, sueldo: ${self.sueldo}, tiempo_trabajo: {self.tiempo_trabajo}]')
+		print(f'Empleado [dui: {self.dui[:8]}-{self.dui[8]}, nit: {self.nit[:4]}-{self.nit[4:10]}-{nit[10:13]}-{nit[13]}, sueldo: ${self.sueldo}, años trabajo: {self.tiempo_trabajo}, aguinaldo: {self.aguinaldo}]')
 
-	def calcular_aguinaldo():
-		if (not self.datos_validos()):
-			return 0.00
+	def calcular_aguinaldo(self):
+		if (self.datos_validos()):
+			dias = 0
+			if (self.tiempo_trabajo >= 1 and self.tiempo_trabajo <= 3):
+				dias = 15
+			if (self.tiempo_trabajo >= 4 and self.tiempo_trabajo <= 10):
+				dias = 19
+			if (self.tiempo_trabajo > 10):
+				dias = 21
+
+			return (self.sueldo / 30 * dias)
+		else:
+			return 0.0
 
 
 dui = input("Ingrese el DUI del empleado (sin guiones): ")
 nit = input("Ingrese el NIT del empleado (sin guiones): ")
 sueldo = float(input("Ingrese el sueldo del empleado: "))
 anios = int(input("Ingrese los años de trabajo: "))
-meses = int(input("Ingrese los meses de trabajo: "))
-dias = int(input("Ingrese los dias de trabajo: "))
 
-emp = Empleado(dui, nit, sueldo, {"anios": anios, "meses": meses, "dias": dias})
+emp = Empleado(dui, nit, sueldo, anios)
 if (emp.datos_validos()):
 	print(emp.to_string())
 else:
